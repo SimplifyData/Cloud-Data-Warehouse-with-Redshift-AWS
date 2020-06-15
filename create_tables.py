dcf6cf4f0e394554367b74d9a56b9a725fb1da39
Atif Zafar <atif.zafar@sds.technology>
2020-06-15 13:06:36 -0400

    Adding print statements to create_table script

--- create_tables.py
+++ create_tables.py
@@ -5,12 +5,14 @@ from sql_queries import create_table_queries, drop_table_queries
 
 def drop_tables(cur, conn):
     for query in drop_table_queries:
+        print('Executing drop query : ' + query)
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
     for query in create_table_queries:
+        print('Executing create table : ' + query)
         cur.execute(query)
         conn.commit()
 
@@ -19,11 +21,17 @@ def main():
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
+    print('Connecting to Redshift')
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+    print('Redshift Connected')
+
     cur = conn.cursor()
 
+    print('Dropping any tables if any')
     drop_tables(cur, conn)
+
     create_tables(cur, conn)
+    print('Tables Created')
 
     conn.close()
 
